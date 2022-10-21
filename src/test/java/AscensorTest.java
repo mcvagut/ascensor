1c1b2fc6cc40ba4ced2a8a6afab4e4348e3f1412
mcvagut <mcvagut@gmail.com>
2022-10-21 02:42:54 -0400

    Agregando 2 Test, para existencia de piso y estado de puerta

--- src/test/java/AscensorTest.java
+++ src/test/java/AscensorTest.java
@@ -24,4 +24,36 @@ public class AscensorTest {
                     3,3,"Abierto");
         }
 
+        //Test para verificar si existe el piso, caso contrario lanzar una excepción.
+        @Test
+        public void verificarExistenciaPisoLanzarExcepcion() throws Exception{
+
+            //Preparación de la prueba
+            Ascensor ascensor = new Ascensor();
+
+            //Lógica de prueba
+            exception.expect(Exception.class);
+            ascensor.SupervisionDeAscensor(1,1,
+                    1,5,"Abierto");
+
+        }
+
+        //Test para verificar el estado de la puerta del ascensor.
+        @Test
+        public void verificarEstadoDePuertaComienzoDia() throws Exception{
+
+            //Preparación de la prueba
+            Ascensor ascensor = new Ascensor();
+
+            //Lógica de prueba
+            ascensor.SupervisionDeAscensor(1,1,
+                    1,3,"Cerrado");
+            exception.expect(Exception.class);
+            ascensor.SupervisionDeAscensor(2,1,
+                    5,3,"Abierto");
+            ascensor.SupervisionDeAscensor(3,1,
+                    2,3,"Abierto");
+        }
+
+
 }
