6fe596f6d9465ae13cf5c21b006e5ae2650ebf66
mcvagut <mcvagut@gmail.com>
2022-10-21 02:29:33 -0400

    Creando función 'SupervicionDeAscensor'

--- src/main/java/Ascensor.java
+++ src/main/java/Ascensor.java
@@ -1,13 +1,20 @@
-
-//Clase Principal
 public class Ascensor {
-    private Character [][] edificio = { {'0'},
-                                        {'0'},
-                                        {'0'}};
-    /** Ejemplo:
-     *
-     * {{'*'},
-     *  {'+'},
-     *  {'-'} };
-     **/
+    private final Character[] piso = {  '0',
+                                        '0',
+                                        '0',};
+
+        //Función para controlar el ascensor, pisos actuales, de persona, destinos, etc
+        public String SupervisionDeAscensor(int AscensorPisoActual, int PersonaPisoActual,
+                                            int limitePersonas, int pisoDestino, String estadoPuerta) throws Exception {
+            crearPersona(PersonaPisoActual);
+            ExcesoPersonas(limitePersonas);
+            ExcesoPisos(pisoDestino);
+            verificarEstadoComienzoDia(AscensorPisoActual,estadoPuerta);
+            return llamarAscensor(PersonaPisoActual,pisoDestino);
+        }
+
+
 }
+
+
+
--- src/test/java/AscensorTest.java
+++ src/test/java/AscensorTest.java
@@ -1,3 +1,12 @@
+import static org.junit.Assert.*;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 public class AscensorTest {
-}
+
+        @Rule
+        public ExpectedException exception = ExpectedException.none();
+
+
+    }
